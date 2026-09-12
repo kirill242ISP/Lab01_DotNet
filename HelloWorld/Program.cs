ff1fdd20b30ac415a6cd0977f7ac6736b2d263e6
kirill242ISP <kigosin48@gmail.com>
2026-09-12 11:32:28 +0300

    laba1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,37 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*int age = 20;
-double gpa = 4.5;
-bool isStudent = true;
-string name = "Иван";
-Console.WriteLine(name);
-Console.WriteLine(age);
-Console.WriteLine(gpa);
-Console.WriteLine(isStudent);*/
+﻿// // See https://aka.ms/new-console-template for more information
+// /*int age = 20;
+// double gpa = 4.5;
+// bool isStudent = true;
+// string name = "Иван";
+// Console.WriteLine(name);
+// Console.WriteLine(age);
+// Console.WriteLine(gpa);
+// Console.WriteLine(isStudent);*/
 
-var firstname = "кирилл";
-var lastname = "игошин";
-var group = "ИСП-242";
-var birthyear = 2008;
-var gpa = 4.6;
-var stipendia = true;
+// var firstname = "кирилл";
+// var lastname = "игошин";
+// var group = "ИСП-242";
+// var birthyear = 2008;
+// var gpa = 4.6;
+// var stipendia = true;
 
-var firstname2 = "наиль";
-var lastname2 = "имашев";
-var group2 = "ИСП-242";
-var birthyear2 = 2008;
-var gpa2 = 4.5;
-var stipendia2 = true;
+// var firstname2 = "наиль";
+// var lastname2 = "имашев";
+// var group2 = "ИСП-242";
+// var birthyear2 = 2008;
+// var gpa2 = 4.5;
+// var stipendia2 = true;
 
-int currentyear = 2026;
-int age = currentyear - birthyear;
+// int currentyear = 2026;
+// int age = currentyear - birthyear;
 
 
-Console.WriteLine("Студенческое удостоверение");
-Console.WriteLine($"Имя: {firstname} {lastname}");
-Console.WriteLine($"гpуnna: {group}");
-Console.WriteLine($"Boзpacт: {age} лет"); Console.WriteLine($"Средний балл: {gpa}");
-Console.WriteLine($"Стипендия: {stipendia}");
+// Console.WriteLine("Студенческое удостоверение");
+// Console.WriteLine($"Имя: {firstname} {lastname}");
+// Console.WriteLine($"гpуnna: {group}");
+// Console.WriteLine($"Boзpacт: {age} лет"); Console.WriteLine($"Средний балл: {gpa}");
+// Console.WriteLine($"Стипендия: {stipendia}");
 
-Console.Write("\nВведите ваш любимый предмет: ");
-string subject = Console.ReadLine();
-Console.WriteLine($"Отлично! {firstname} любит {subject}.");
+// Console.Write("\nВведите ваш любимый предмет: ");
+// string subject = Console.ReadLine();
+// Console.WriteLine($"Отлично! {firstname} любит {subject}.");
+int a = 15;
+int b = 4;
+Console.WriteLine($"Cyммa: {a + b}"); Console.WriteLine($"Paзность: {a - b}"); Console.WriteLine($"Произведение:{a * b}");
+Console.WriteLine($"чacтное (int): {a / b}"); Console.WriteLine($"Oстaток: {a % b}");
