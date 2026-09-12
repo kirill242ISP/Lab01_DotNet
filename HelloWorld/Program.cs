e1be729ccbef6cf83caeac2ad7d3ad73109b1e9c
kirill242ISP <kigosin48@gmail.com>
2026-09-12 11:01:39 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет от ИСП-242!");
+Console.WriteLine("Автор: Игошин Кирилл и Имашев Наиль");
+Console.WriteLine("Год: 2026");
