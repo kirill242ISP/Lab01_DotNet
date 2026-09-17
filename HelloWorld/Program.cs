193b3773e9fc4b0ab1d3ad0ad200e7f8263c82c9
anubis3333333333 <jdkdoekd471@gmail.com>
2026-09-17 14:45:03 +0300

    Step 9: complete student questionnaire program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -41,15 +41,44 @@
 // Console.WriteLine($"чacтное (int): {a / b}"); Console.WriteLine($"Oстaток: {a % b}");
 // double result = (double)a / b;
 // Console.WriteLine($"Частное (double):{result}");
-Console.WriteLine("Калькулятор");
-Console.WriteLine("Введите первое число:");
-double num1 = double.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-double num2 = double.Parse(Console.ReadLine());
-Console.WriteLine($"Сумма:{num1 + num2}");
-Console.WriteLine($"Разность:{num1 - num2}");
-Console.WriteLine($"Произведение:{num1 * num2}");
-if (num2 != 0)
-    Console.WriteLine($"Частное:{num1 / num2}");
-else
-    Console.WriteLine("Деление на ноль невозможно");
+// Console.WriteLine("Калькулятор");
+// Console.WriteLine("Введите первое число:");
+// double num1 = double.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// double num2 = double.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма:{num1 + num2}");
+// Console.WriteLine($"Разность:{num1 - num2}");
+// Console.WriteLine($"Произведение:{num1 * num2}");
+// if (num2 != 0)
+//     Console.WriteLine($"Частное:{num1 / num2}");
+// else
+//     Console.WriteLine("Деление на ноль невозможно");
+// int metr1 = 1;
+// System.Int32 metr2 = 1;
+Console.WriteLine("Добро пожаловать в анкету!");
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.Write("Введите вашу фамилию: ");
+string surname = Console.ReadLine();
+Console.Write("Введите вашу группу: ");
+string group = Console.ReadLine();
+Console.Write("Введите ваш год рождения: ");
+int birthYear = int.Parse(Console.ReadLine());
+Console.Write("Введите ваш cредний балл: ");
+double gpa = double.Parse(Console.ReadLine());
+int currentYear = 2026;
+int age = currentYear - birthYear;
+bool isExcellent = gpa >= 4.5;
+// string status;
+// if (isExcellent == true) status = "Отличник";
+// else status = "Хорошист";
+string status = isExcellent ? "Отличник" : "Хорошист";
+Console.WriteLine("Ваша анкета");
+Console.WriteLine($"Имя: {name} {surname}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {age} лет");
+Console.WriteLine($"Средний балл: {gpa} ");
+Console.WriteLine($"Статус: {status} ");
+Console.WriteLine($"Лет до 30: {30 - age}");
+Console.WriteLine("Нажмите enter для выхода....");
+Console.ReadLine();
