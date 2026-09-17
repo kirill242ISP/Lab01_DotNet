d2a80230b70af507523319224e46f41306647bb2
anubis3333333333 <jdkdoekd471@gmail.com>
2026-09-17 14:18:51 +0300

    Step 8: arithmetic and calculator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -35,7 +35,21 @@
 // Console.Write("\nВведите ваш любимый предмет: ");
 // string subject = Console.ReadLine();
 // Console.WriteLine($"Отлично! {firstname} любит {subject}.");
-int a = 15;
-int b = 4;
-Console.WriteLine($"Cyммa: {a + b}"); Console.WriteLine($"Paзность: {a - b}"); Console.WriteLine($"Произведение:{a * b}");
-Console.WriteLine($"чacтное (int): {a / b}"); Console.WriteLine($"Oстaток: {a % b}");
+// int a = 15;
+// int b = 4;
+// Console.WriteLine($"Cyммa: {a + b}"); Console.WriteLine($"Paзность: {a - b}"); Console.WriteLine($"Произведение:{a * b}");
+// Console.WriteLine($"чacтное (int): {a / b}"); Console.WriteLine($"Oстaток: {a % b}");
+// double result = (double)a / b;
+// Console.WriteLine($"Частное (double):{result}");
+Console.WriteLine("Калькулятор");
+Console.WriteLine("Введите первое число:");
+double num1 = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма:{num1 + num2}");
+Console.WriteLine($"Разность:{num1 - num2}");
+Console.WriteLine($"Произведение:{num1 * num2}");
+if (num2 != 0)
+    Console.WriteLine($"Частное:{num1 / num2}");
+else
+    Console.WriteLine("Деление на ноль невозможно");
